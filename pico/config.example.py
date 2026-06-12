ffa8c84bfc5b3bf4e27f19f93c2b2b470390c7da
NicoSjokk <nicol@stud.ntnu.no>
2026-06-13 01:05:45 +0200

    Cleanup, formating and abstracting out broker and wifi settings

---
+++ pico/config.example.py
@@ -0,0 +1,8 @@
+WIFI_SSID = "your_wifi_name"
+WIFI_PASSWORD = "your_wifi_password"
+
+MQTT_BROKER = "192.168.1.100"
+MQTT_PORT = 1883
+
+CLIENT_ID = "pico_environment_sensor"
+TOPIC = b"idc/pico/environment"
